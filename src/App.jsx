d3b80da7d3b8ff12605cdd02aedd45907f0ccdba
Anuj192006 <anuju712@gmail.com>
2025-04-05 15:42:46 +0530

    loader added

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
+// src/App.js
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar.jsx';
 import Home from './Components/Home/Home.jsx';
 import Portfolio from './Components/Portfolio/Portfolio.jsx';
 import Body from './Components/Addtask/Body.jsx';
-import './index.css';
 import Login from './Login/Login.jsx';
+import Loader from './Components/Loader/Loader.jsx'; // Import the Loader component
+import './index.css';
 
 function App() {
   return (
     <Router>
       <div className="app-container">
         <Routes>
-          <Route path="/" element={<Login/>} />
+          <Route path="/" element={<Loader />} /> 
           <Route path="/home" element={<Home />} />
           <Route path="/add-task" element={<Body />} />
           <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </div>
     </Router>
---
+++ src/Components/Loader/Loader.jsx
@@ -0,0 +1,39 @@
+// src/Components/Loader/Loader.jsx
+
+import React, { useState, useEffect } from 'react';
+import { Navigate, useNavigate } from 'react-router-dom';
+import './Loader.css';
+
+const Loader = () => {
+    const [currentText, setCurrentText] = useState();
+    const [showContent, setShowContent] = useState(false);
+    const texts = ["Set Tasks", "Achieve It", "Stay Motivated"];
+
+    useEffect(() => {
+        let textIndex = 0;
+        const textInterval = setInterval(() => {
+            if (textIndex < texts.length) {
+                setCurrentText(texts[textIndex]);
+                textIndex++;
+            } else {
+                clearInterval(textInterval);
+                setShowContent(true); 
+            }
+        }, 1000);
+        return () => clearInterval(textInterval);
+    },[]);
+
+    return (
+        <div className="loader-container">
+            {!showContent ? (
+                <div className="loader">
+                    <p>{currentText}</p>
+                </div>
+            ) : (
+                <Navigate to="/login" />
+            )}
+        </div>
+    );
+};
+
+export default Loader;
